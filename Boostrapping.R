3a5ca4f3e01e11982ebddbe684be4c56bea52dc8
juanpablohdz <40364606+juanpablohdz@users.noreply.github.com>
2020-11-16 14:16:21 -0600

    Update Boostrapping.R

--- Boostrapping.R
+++ Boostrapping.R
@@ -1,7 +1,7 @@
 A<-c(20,120,130,1,3,4,55,6,22,74,17,29,32,56,42,56,36,2,67,11,10,24,17) # numeros a ocupar
 
 
-library(boot)#la libreria se ocupara para utilizar el metodo estadististico boostraping
+library(boot)#la libreria se ocupara para utilizar el metodo estadistico boostraping
 #cuando se corre  sustituir mean por var
 F<- function(A,d){return(var(A[d]))} # se guarda una funcion para su analisis y ocuparla en diferentes situaciones
 View(F) #muestra de la funcion 
